a833983fac285741b5686df7bf9fb6db40f5c6ba
tonmoypramanik <tonmoy@friendship-bd.org>
2016-11-23 10:40:16 +0600

    SP time Schedule Rename

---
+++ grails-app/domain/com/model/ListSpTimeScheduleActionServiceModel.groovy
@@ -0,0 +1,21 @@
+package com.model
+
+class ListSpTimeScheduleActionServiceModel {
+    public static final String MODEL_NAME = 'list_sp_time_schedule_action_service_model'
+
+    public static final String SQL_LIST_MISSION_MODEL = """
+        CREATE OR REPLACE VIEW list_sp_time_schedule_action_service_model AS
+        SELECT id,version,from_date,to_date,description FROM sp_time_schedule;
+    """
+
+    long id
+    long version
+    Date fromDate
+    Date toDate
+    String description
+
+    static mapping = {
+        cache usage: "read-only"
+    }
+
+}
