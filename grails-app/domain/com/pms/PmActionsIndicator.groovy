d1c9e5d7ccffdf8cb7a160a214a76ea5d07274c9
tonmoypramanik <tonmoy@friendship-bd.org>
2016-12-11 12:59:37 +0600

    projects

---
+++ grails-app/domain/com/pms/PmActionsIndicator.groovy
@@ -0,0 +1,13 @@
+package com.pms
+
+class PmActionsIndicator {
+    long id
+    long version
+    Long actionsId
+    Long monthlyIndicatorId
+    String indicator
+    Double target
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/com/pms/PmActionsIndicatorDetails.groovy
@@ -0,0 +1,16 @@
+package com.pms
+
+class PmActionsIndicatorDetails {
+    long id
+    long version
+    Long indicatorId
+    String monthName
+    Double target
+    Date createDate
+    long createBy
+
+    static mapping = {
+    }
+    static constraints = {
+    }
+}
