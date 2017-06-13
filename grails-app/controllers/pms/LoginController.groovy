627154bc1036343dcd4574d182b175719eaa5b78
zakariaimtiaz <imtiaz71985@gmail.com>
2017-06-13 17:04:19 +0600

    update

--- grails-app/controllers/pms/LoginController.groovy
+++ grails-app/controllers/pms/LoginController.groovy
@@ -1,5 +1,6 @@
 package pms
 
+import com.logic27.awls.crypto.CryptoUtil
 import com.pms.SecUser
 import grails.converters.JSON
 import grails.plugin.springsecurity.SpringSecurityUtils
@@ -10,6 +11,7 @@ import org.springframework.security.authentication.DisabledException
 import org.springframework.security.authentication.LockedException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.WebAttributes
+import pms.utility.Tools
 import service.PmActionsService
 
 import javax.servlet.http.HttpServletResponse
@@ -230,6 +232,47 @@ class LoginController extends BaseController {
         map.put('data',list)
         render map as JSON
 
+    }
+
+    def showEncp(){
+        render(view: '/login/showEncp')
+    }
+    def encryptTxt(){
+        String txt1 = params.input1
+        String txt2 = params.input2
+
+        CryptoUtil cryptoUtil = new CryptoUtil();
+        String output1 = txt1.equals('')?'':cryptoUtil.encrypt(Tools.FLASK,txt1);
+        String output2 = txt2.equals('')?'':cryptoUtil.encrypt(Tools.FLASK,txt2);
+        def map = [:]
+        map.put('output1',output1)
+        map.put('output2',output2)
+        render map as JSON
+
+    }
+    def decryptTxt(){
+        String txt1 = params.input1
+        String txt2 = params.input2
+        String output1
+        String output2
+
+        CryptoUtil cryptoUtil = new CryptoUtil();
+        try {
+            output1 = cryptoUtil.decrypt(Tools.FLASK,txt1);
+        } catch (Exception ex) {
+            output1 = cryptoUtil.encrypt(Tools.FLASK,txt1);
+            output1 = cryptoUtil.decrypt(Tools.FLASK,output1);
+        }
+        try {
+            output2 = cryptoUtil.decrypt(Tools.FLASK,txt2);
+        } catch (Exception ex) {
+            output2 = cryptoUtil.encrypt(Tools.FLASK,txt2);
+            output2 = cryptoUtil.decrypt(Tools.FLASK,output2);
+        }
+        def map = [:]
+        map.put('output1',output1)
+        map.put('output2',output2)
+        render map as JSON
     }
 
 }
